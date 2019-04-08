08f32005bfe1a03e668c65473bccd2042493c7a9
Peter Starup <pete59f1@edu.eal.dk>
2019-04-08 10:35:44 +0200

    Øvelse 4

--- Ex72-Threads01/Program.cs
+++ Ex72-Threads01/Program.cs
@@ -9,7 +9,9 @@ namespace Ex72_Threads01
 {
     class Program
     {
-        public delegate void ThreadStart();
+        private char _sharedChar;
+        private const int SIMULATE_WORK = 100;
+
         static void Main(string[] args)
         {
             Thread t = new Thread(WriteHello);
@@ -27,6 +29,9 @@ namespace Ex72_Threads01
             t.Start("Hej t");
             t2.Start("Nej t2");
             t3.Start("Hello World");
+
+            Program p = new Program();
+            p.Run();
         }
 
         static void WriteHello(Object mess)
@@ -34,6 +39,42 @@ namespace Ex72_Threads01
             for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine(mess.ToString());
+            }
+        }
+
+        public void Run()
+        {
+            Thread tA = new Thread(WriteA);
+            Thread tB = new Thread(WriteB);
+            tA.Name = "Thread A";
+            tB.Name = "Thread B";
+            tA.Start();
+            tB.Start();
+            tA.Join();
+            tB.Join();
+            Console.Write("\nPress a key ....");
+            Console.ReadKey();
+        }
+
+        private void WriteA()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _sharedChar = 'A';
+                Thread.Sleep(SIMULATE_WORK);
+                Console.WriteLine($"{Thread.CurrentThread.Name} : {_sharedChar}");
+                Thread.Yield();
+            }
+        }
+
+        private void WriteB()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _sharedChar = 'B';
+                Thread.Sleep(SIMULATE_WORK);
+                Console.WriteLine($"{Thread.CurrentThread.Name} : {_sharedChar}");
+                Thread.Yield();
             }
         }
     }
